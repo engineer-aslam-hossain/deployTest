2593d899da5adafaf5da2677fc33db415069fc0c
engineer-aslam-hossain <engineer.aslamhossain@gmail.com>
2021-03-06 17:28:41 +0600

    make some changes

--- src/pages/findDoctors/index.js
+++ src/pages/findDoctors/index.js
@@ -271,7 +271,7 @@ const FindDoctors = ({ doctors }) => {
 
 export default FindDoctors;
 
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
   const res = await fetch(
